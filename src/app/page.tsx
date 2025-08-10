8e4e9d7cd756c959877879f7ad8ced1ef5549892
Navkirat singh <132665095+Vanillapapa64@users.noreply.github.com>
2025-08-10 14:17:35 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,11 +23,14 @@ export default function Home() {
           </div>
           <div style={{ fontFamily: 'var(--font-swit-display)' }} className="flex justify-center md:justify-start  gap-4 text-xl md:text-2xl xl:text-4xl ">
             <div className=" font-light">Aka</div>
-            <a href="https://github.com/Vanillapapa64" target="_blank" style={{ fontFamily: 'var(--font-clash-display)' }} className="font-medium text-gray-500 flex gap-1 underline underline-offset-4 cursor-pointer hover:text-white">Vanillapapa64 <Image
-              src="/redirect.png"
-              width={38}
-              height={38}
-              alt="Picture of the author"/></a>
+            <a href="https://github.com/Vanillapapa64" target="_blank" style={{ fontFamily: 'var(--font-clash-display)' }} className="font-medium text-gray-500 flex gap-1 underline underline-offset-4 cursor-pointer hover:text-white">Vanillapapa64 <div className="relative w-8 flex justify-start sm:w-8 md:w-8 h-auto">
+              <Image
+                src="/redirect.png"
+                alt="Picture of the author"
+                fill
+                className="object-contain"
+              />
+            </div></a>
           </div>
           <div className="md:pt-4 md:text-2xl text-center md:text-left font-light text-slate-300 tracking-widest">WEB | ML | DEV OPS | DS</div>
         </div>
